e0a372fcd51cd000681b7b347e0d971591cc646b
Muhammadaziz <muhammadazizdev@gmail.com>
2024-10-31 12:52:05 +0600

    update

--- api/carts/views.py
+++ api/carts/views.py
@@ -23,7 +23,7 @@ class CardsViewSet(CartsModelMixin):
     permission_classes_by_activ = {
         'list': [IsAuthenticated],
         'retrieve': [IsAuthenticated],
-        'create': [IsAuthenticated, IsAdminUser],
+        'create': [IsAuthenticated | IsAdminUser],
         'update': [IsAuthenticated, IsOwnerUser | IsAdminUser],
         'destroy': [IsAuthenticated, IsOwnerCard | IsAdminUser],
     }
