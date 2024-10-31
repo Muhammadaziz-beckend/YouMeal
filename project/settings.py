98aba58d8fd1d45de14284178f0a80180d47ce41
Muhammadaziz <muhammadazizdev@gmail.com>
2024-10-31 12:24:27 +0600

    add cors

--- project/settings.py
+++ project/settings.py
@@ -148,7 +148,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # для локальной разработки React
+    'https://youmealfront.onrender.com',  # для продакшен-окружения
+]
 
 CORS_ALLOW_HEADERS = (
     "accept",
